e00753b285a2ce9fcd0051f4afd78a7b3243807f
tuoyu <tuoyu@staff.weibo.com>
2017-12-19 14:27:05 +0800

    feature reading: finish timer.h timer.cpp

--- timer.h
+++ timer.h
@@ -1,12 +1,22 @@
 #include <chrono>
 
+/**
+ * 记录间隔时间的Class，用来记录间隔时间
+ */
 class Timer
 {
 public:
+    /** Construct **/
     Timer();
     void reset();
+    
+    /** refresh timer start **/
     void tic();
+
+    /** refresh duration **/
     float toc();
+
+    /** return duration long **/
     float get();
 private:
     std::chrono::high_resolution_clock::time_point begin;
